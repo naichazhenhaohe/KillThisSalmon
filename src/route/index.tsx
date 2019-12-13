de7fd0992c549b315cdfc6c478f2b13fbea44706
wangjchs <wangjchs@yonyou.com>
2019-12-13 15:33:10 +0800

    -

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "@com/Header";
-import Footer from "@com/Footer";
-import SalmonRun from "@/pages/salmonRun";
-import TurfWar from "@/pages/turfWar";
-import RankBattle from "@/pages/rankBattle";
-import LeagueBattle from "@/pages/leagueBattle";
-import SplatNetGear from "@/pages/splatNetGear";
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from '@com/Header'
+import Footer from '@com/Footer'
+import SalmonRun from '@/pages/salmonRun'
+import TurfWar from '@/pages/turfWar'
+import RankBattle from '@/pages/rankBattle'
+import LeagueBattle from '@/pages/leagueBattle'
+import SplatNetGear from '@/pages/splatNetGear'
 import { myTheme } from '@/globalcss/myTheme'
-import styled from 'styled-components'
-
-const StyledRouter = styled(Router)``
+import { ThemeProvider } from 'styled-components'
 
 export default class App extends React.Component {
   render() {
     return (
-      <StyledRouter theme={myTheme}>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={SalmonRun}></Route>
-          <Route path="/turfWar" component={TurfWar}></Route>
-          <Route path="/rankBattle" component={RankBattle}></Route>
-          <Route path="/leagueBattle" component={LeagueBattle}></Route>
-          <Route path="/splatNetGear" component={SplatNetGear}></Route>
-        </Switch>
-        <Footer />
-      </StyledRouter>
-    );
+      <ThemeProvider theme={myTheme}>
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={SalmonRun}></Route>
+            <Route path="/turfWar" component={TurfWar}></Route>
+            <Route path="/rankBattle" component={RankBattle}></Route>
+            <Route path="/leagueBattle" component={LeagueBattle}></Route>
+            <Route path="/splatNetGear" component={SplatNetGear}></Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </ThemeProvider>
+    )
   }
 }
